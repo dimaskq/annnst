ebbc00f0605da5419f7027ea76fd802f2b58f46c
dimaskq <dimassskq@gmail.com>
2023-11-07 21:27:25 +0200

    Debugging

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,24 @@
-
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
-    spaceBetween: 10,
-    effect: 'flip',
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    history: {
-        replaceState: true,
-      },
-  });
+  // Optional parameters
+  // direction: 'vertical',
+  effect: 'flip',
+  loop: true,
+  limitRotation: true,
+  slideShadows: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
