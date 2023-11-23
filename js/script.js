fcda884f686d53170043c55b04a199dfcfee098f
dimaskq <dimassskq@gmail.com>
2023-11-23 18:15:55 +0200

    schedules

--- js/script.js
+++ js/script.js
@@ -23,3 +23,48 @@ function burgerMenu(selector) {
 }
 
 burgerMenu ('.burger-menu');
+
+
+var acc = document.getElementsByClassName("accordion");
+const acc2 = document.querySelector('.accordion > span');
+let topics = document.querySelector('.topics__container')
+var i;
+let acrd = document.getElementById("acrd");
+let marginTop = document.getElementById("marginTop");
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    acc2.innerHTML = (acc2.innerHTML === 'Читати далі...') ? acc2.innerHTML = 'Сховати' : acc2.innerHTML = 'Читати далі...';
+    topics.classList.toggle("pdbottom")
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+    acrd.classList.toggle("topics__box_accordion");
+    marginTop.classList.toggle("marginTop")
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "flex") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "flex";
+    }
+  });
+}
+
+var acc3 = document.getElementsByClassName("accordion2");
+var h;
+
+for (h = 0; h < acc3.length; h++) {
+  acc3[h].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel2 = this.nextElementSibling;
+    if (panel2.style.display === "block") {
+      panel2.style.display = "none";
+    } else {
+      panel2.style.display = "block";
+    }
+  });
+}
